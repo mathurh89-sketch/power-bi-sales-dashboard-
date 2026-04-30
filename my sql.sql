c7a27d9208c9c8deb6e85490853d8eddac9ed3ef
Harshit Mathur <mathurh89@gmail.com>
2026-04-30 12:33:34 +0530

    Add files via upload

---
+++ my sql.sql
@@ -0,0 +1,7 @@
+employee2create database ;
+
+create table student (name varchar (30) , rollno int , subject varchar (30) , ;
+
+insert into student values ("rahul" , 3 "maths") ; 
+
+select * from student 
